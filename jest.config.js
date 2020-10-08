a6a44411ef641c754a0241a8afb6ca9a2cdec7be
pozil <philippe.ozil@gmail.com>
2020-10-08 09:45:11 +0200

    Stopped overriding setupFilesAfterEnv

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
+const setupFilesAfterEnv = jestConfig.setupFilesAfterEnv || [];
+setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
         '^lightning/flowSupport$': '<rootDir>/jest-mocks/lightning/flowSupport'
     },
-    setupFilesAfterEnv: ['<rootDir>/jest-sa11y-setup.js']
+    setupFilesAfterEnv
 };
